a8088314a92573220202fac75bd91e3404552cce
Tims Tittus <151863700+TimsTittus@users.noreply.github.com>
2024-03-20 17:40:19 +0530

    Add files via upload

---
+++ pattern.c.c
@@ -0,0 +1,11 @@
+//to print a given triangular star pattern
+#include<stdio.h>
+int main()
+{
+    printf("     *\n");
+    printf("    * *\n");
+    printf("   * * *\n");
+    printf("  * * * *\n");
+    printf(" * * * * *\n");
+    printf("* * * * * *\n");
+}
